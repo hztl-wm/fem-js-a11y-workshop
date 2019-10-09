0847df39a7b9ced36a3f3030e67808c505c9748a
William Harrison <wharrison@horizontalintegration.com>
2019-10-09 14:28:57 -0500

    added assertion test for Cypress according to workshop

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,1 +1,12 @@
 /// <reference types="Cypress" />
+
+describe('Accessibility tests', () => {
+
+    it('should send foocus to a skip link when the route changes from a link click', () => {
+        cy.visit('http://localhost:8000') // check the local project, could admittedly be more dynamic i.e. if you have more than one instance of Gatsby running, maybe the test should check where this projecy is running instead of being hard-coded
+
+        cy.get('#page-navigation li:first-of-type a').click() // find the element with that id, that is in a li, that is a link
+
+        cy.focused().should('have.class', 'routeSkipLink') // check whether the focused element has the class 'routeSkipLink' 
+    })
+})
